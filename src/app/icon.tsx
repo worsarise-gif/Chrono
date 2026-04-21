24ac16130eb36f7ecd7e78ce80470ed6cf2cbdcc
worsarise-gif <worsarise@gmail.com>
2026-04-21 09:29:40 -0400

    Update icon.tsx

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -1,6 +1,6 @@
 import { ImageResponse } from 'next/og'
 
-// Image metadata
+// Image metadata - Project Chrono
 export const size = {
   width: 32,
   height: 32,
@@ -33,18 +33,30 @@ export default function Icon() {
             justifyContent: 'center',
           }}
         >
-          <span
+          {/* Rotated wrapper for Satori compatibility */}
+          <div
             style={{
-              fontSize: 20,
-              fontWeight: 900,
-              color: '#1a1a1a',
-              fontFamily: 'Arial, Helvetica, sans-serif',
-              lineHeight: 1,
-              transform: 'translateY(1px)', // slight visual centering tweak
+              display: 'flex',
+              transform: 'rotate(-45deg)',
             }}
           >
-            C
-          </span>
+            <svg
+              width="26"
+              height="26"
+              viewBox="0 0 100 100"
+            >
+              {/* Top-left piece with mathematically mapped flare curve */}
+              <path
+                d="M 2 45 L 32.7 45 A 18 18 0 0 1 67.3 45 L 85.6 45 A 36 36 0 0 0 25.3 25.3 C 17.3 33.3, 10 44, 2 45 Z"
+                fill="#1a1a1a"
+              />
+              {/* Bottom-right piece (exact 180 degree rotation of the top-left) */}
+              <path
+                d="M 98 55 L 67.3 55 A 18 18 0 0 1 32.7 55 L 14.4 55 A 36 36 0 0 0 74.7 74.7 C 82.7 66.7, 90 56, 98 55 Z"
+                fill="#1a1a1a"
+              />
+            </svg>
+          </div>
         </div>
       </div>
     ),
